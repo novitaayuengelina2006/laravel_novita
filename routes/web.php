173507f5ca4909e0c024470c653819ed6ee975bd
novitaayuengelina2006 <engelinenovita@gmail.com>
2024-04-19 15:30:36 +0700

    first comit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// coute basic
+Route::get('/about', function () {
+    return '<h1>Halo<h1>'
+    . 'selamat datang  <br>'
+    . 'laravel, emang keren.' ;
+});
+
+Route::get('animals', function () {
+    $king = "lion";
+    $hewan = ["monkey", "dragonfly", "tiger", "butterfly", "crocodile"];
+    return view('animals_page', compact('king', 'hewan'));
+});
